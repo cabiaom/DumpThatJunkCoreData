6c6adee91527a0c21043baca53361bd9e310cd83
cabiaom <cabiaom@seattleu.edu>
2016-04-15 10:46:25 -0700

    Added Search
    - Search works for locations
    - Add works for locations
    - No pictures yet

--- DumpThatJunkCoreData/AppDelegate.swift
+++ DumpThatJunkCoreData/AppDelegate.swift
@@ -14,9 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        /*
         let splitViewController = window!.rootViewController as! UISplitViewController
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
@@ -25,23 +25,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         UISearchBar.appearance().barTintColor = UIColor.candyGreen()
         UISearchBar.appearance().tintColor = UIColor.whiteColor()
         UITextField.appearanceWhenContainedInInstancesOfClasses([UISearchBar.self]).tintColor = UIColor.candyGreen()
-        return true
-    }
-    
-    // MARK: - Split view
-    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
-        /*
-        guard let secondaryAsNavController = secondaryViewController as? UINavigationController
-            else { return false }
-        guard let topAsDetailController = secondaryAsNavController.topViewController as? AddTableViewController
-            else { return false }
-        
-        if topAsDetailController.detailCandy == nil {
-            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-            return true
-        }
+         
          */
-        return false
+        return true
     }
     
     func applicationWillResignActive(application: UIApplication) {
---
+++ DumpThatJunkCoreData/SearchViewController.swift
@@ -0,0 +1,133 @@
+//
+//  SearchViewController.swift
+//  DumpThatJunkCoreData
+//
+//  Created by Mark CABIAO on 4/14/16.
+//  Copyright © 2016 Mark CABIAO. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate{
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var names = [String]()
+    var locationNames = [NSManagedObject]()
+    
+    var searchActive : Bool = false
+    //var data = ["San Francisco","New York","San Jose","Chicago","Los Angeles","Austin","Seattle"]
+    var filtered:[String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* Setup delegates */
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        
+        fetchAllLocations()
+        
+    }
+    
+    func fetchAllLocations(){
+        let appDelegate    = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let fetchRequest   = NSFetchRequest(entityName: "Location")
+        
+        do{
+            let fetchedResult = try managedContext.executeFetchRequest(fetchRequest) as? [NSManagedObject]
+            
+            if let results = fetchedResult{
+                locationNames = results
+                
+                for object in locationNames{
+                    names.append((object.valueForKey("name") as? String)!)
+                }
+                
+            }
+            else{
+                print("Could not fetch result")
+            }
+        }
+        catch{
+            print("There is some error.")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        // change to - location names
+        filtered = names.filter({ (text) -> Bool in
+            let tmp: NSString = text
+            let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
+            return range.location != NSNotFound
+        })
+        if(filtered.count == 0){
+            searchActive = false;
+        } else {
+            searchActive = true;
+        }
+        self.tableView.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(searchActive) {
+            return filtered.count
+        }
+        return locationNames.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")! as UITableViewCell;
+        cell.accessoryType = .DisclosureIndicator
+        
+        if(searchActive){
+            cell.textLabel?.text = filtered[indexPath.row]
+        } else {
+            cell.textLabel?.text = names[indexPath.row]
+            /*
+            let room = locationNames[indexPath.row]
+            let nameInCell = room.valueForKey("name") as? String
+            cell.textLabel?.text = nameInCell
+            names.append(nameInCell!)
+             */
+        }
+        
+        return cell;
+    }
+
+
+}
